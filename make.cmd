c4f87ec7efbba79b10861f1960b6f1cf1a03ebf4
Peter Ju <peterju.tw@gmail.com>
2024-11-18 09:01:00 +0800

    調整nuitka打包參數

--- make.cmd
+++ make.cmd
@@ -1,4 +1,10 @@
 @echo off
 REM --output-filename=
 REM Set-ExecutionPolicy Unrestricted
-python -m nuitka --onefile --enable-plugin=tk-inter --windows-icon-from-ico=chklink.ico --output-dir=out chklink.py
+
+REM 刪除 out 目錄內的所有檔案與目錄
+if exist out (
+    rmdir /s /q out
+)
+
+nuitka --standalone --onefile --enable-plugin=tk-inter --windows-icon-from-ico=chklink.ico --output-dir=out - -windows-console-mode=disable chklink.py
