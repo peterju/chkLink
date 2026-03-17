9fdc17399d6c978b81bb910f7f5444ddb3fbcb97
peterju <peterju.tw@gmail.com>
2026-03-17 10:02:49 +0800

    補充自然人憑證簽章與更新說明，並整理 pycert 腳本

--- pycert.ps1
+++ pycert.ps1
@@ -1,1 +1,26 @@
-..\SignTool\x64\signtool.exe sign /sha1 089a46b557607ae3bf629b07906b8931088107f3 /fd SHA1 /t http://timestamp.sectigo.com /v out\chklink.exe
+$ErrorActionPreference = 'Stop'
+
+$signToolPath = '..\SignTool\x64\signtool.exe'
+$thumbprint = '63dc665f1795f66146cf1096d956fd797060af24'
+$timestampUrl = 'http://timestamp.sectigo.com'
+$targetFile = 'out\chklink.exe'
+
+if (-not (Test-Path -LiteralPath $signToolPath)) {
+    Write-Host '[錯誤] 找不到 signtool.exe，請先安裝 Windows SDK，或修改 pycert.ps1 內的 $signToolPath。' -ForegroundColor Red
+    exit 1
+}
+
+if (-not (Test-Path -LiteralPath $targetFile)) {
+    Write-Host '[錯誤] 找不到 out\chklink.exe，請先執行 make.cmd 產生執行檔。' -ForegroundColor Red
+    exit 1
+}
+
+Write-Host '開始對 out\chklink.exe 進行簽章...'
+& $signToolPath sign /sha1 $thumbprint /fd SHA1 /t $timestampUrl /v $targetFile
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host '[錯誤] SignTool 執行失敗，請確認憑證、thumbprint 與 signtool.exe 路徑設定。' -ForegroundColor Red
+    exit $LASTEXITCODE
+}
+
+Write-Host '[完成] chklink.exe 已完成簽章。' -ForegroundColor Green
